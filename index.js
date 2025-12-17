6ac6d143c0cdaf11fdb83d4de427f2325af696fa
AbdallahDev <abdallah.dev85@gmail.com>
2025-12-17 11:09:05 +0000

    list

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const min = 2;
-const max = 11;
-const range = max - min + 1;
 var hasBlackJack = false;
 var isAlive = true;
 var msg = "";
-const randomNum1 = Math.random();
-const randomNum2 = Math.random();
-const firstCard = Math.floor(randomNum1 * range) + min;
-const secondCard = Math.floor(randomNum2 * range) + min;
-const cards = [firstCard, secondCard];
+const cards = [];
 var sum = 0;
 
 const messageEl = document.getElementById("message-el");
 const sumEl = document.querySelector("#sum-el");
 const cardsEl = document.querySelector("#cards-el");
+cardsEl.textContent = "Cards: ";
 
 function cardsSum() {
   sum = cards.reduce((accumulator, current) => accumulator + current, 0);
@@ -21,12 +15,15 @@ function cardsSum() {
 }
 
 function startGame() {
+  for (let i = 0; i < 2; i++) {
+    drawCard();
+  }
+  console.log(cards);
   renderGame();
 }
 
 function renderGame() {
   cardsSum();
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
 
   if (sum < 21) msg = "Do you want to draw a new card?";
   else if (sum === 21) {
@@ -36,19 +33,18 @@ function renderGame() {
     msg = "You're out of the game!";
     isAlive = false;
   }
-  console.log(sum, msg);
+  //   console.log(sum, msg);
   messageEl.textContent = msg;
 }
 
 function drawCard() {
+  const min = 2;
+  const max = 11;
+  const range = max - min + 1;
   const randomNum = Math.random();
   const newCard = Math.floor(randomNum * range) + min;
-  console.log(randomNum, Math.floor(randomNum * range), newCard);
+  cards.push(newCard);
 
-  sum += newCard;
-  sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent =
-    "Cards: " + firstCard + " " + secondCard + " " + newCard;
-
+  cardsEl.append(newCard + " ");
   renderGame();
 }
