233bffa6108adffc50756df705a52283bd3fdca6
AbdallahDev <abdallah.dev85@gmail.com>
2025-12-17 10:36:36 +0000

    array

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const min = 2;
 const max = 11;
 const range = max - min + 1;
-// const randomNum1 = Math.random();
-// const randomNum2 = Math.random();
 var hasBlackJack = false;
 var isAlive = true;
 var msg = "";
@@ -10,11 +8,8 @@ const randomNum1 = Math.random();
 const randomNum2 = Math.random();
 const firstCard = Math.floor(randomNum1 * range) + min;
 const secondCard = Math.floor(randomNum2 * range) + min;
-var sum = firstCard + secondCard;
-
-// const firstCard = Math.floor(randomNum1 * range) + min;
-// const secondCard = Math.floor(randomNum2 * range) + min;
-// var sum = firstCard + secondCard;
+const cards = [firstCard, secondCard];
+var sum = 0;
 
 const messageEl = document.getElementById("message-el");
 const sumEl = document.querySelector("#sum-el");
@@ -25,6 +20,7 @@ function startGame() {
 }
 
 function renderGame() {
+    
   sumEl.textContent = "Sum: " + sum;
   cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
 
